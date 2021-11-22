fd1af882a52c5f3a053c67f18f64e0fa389158be
Tyler Rice <tylerrice121@gmail.com>
2021-11-22 15:00:42 -0600

    wip: adds secret key and first model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class UserEntries(models.Model):
+    title = models.CharField(max_length=100)
+    entry = models.CharField(max_length=350)
+    img = models.CharField(max_length=250)
+    video_url = models.CharField(max_length=250)
+    date = models.DateField('Post Date')
+
+    def __str__(self):
+        return f"{self.title} was created on {self.date} with the id of {self.id}"
+
+    
