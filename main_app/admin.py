587b675c11377ba9b90cbcefd74037e2c37536b0
Tyler Rice <tylerrice121@gmail.com>
2021-11-23 12:03:01 -0600

    wip adds feed datat

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserEntries
+from .models import UserEntries, AllEntries
 
 # Register your models here.
 
 admin.site.register(UserEntries)
+admin.site.register(AllEntries)
