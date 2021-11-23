834dfdda796a0a339169246c67b5cec91049c053
Tyler Rice <tylerrice121@gmail.com>
2021-11-23 10:30:30 -0600

    wip: adds profile page

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('feed/', views.feed, name='feed'),
+    path('profile/', views.Profile.as_view(), name='profile')
 ]
