e7dfd7b5ea9bc4eb62fdf0219ff11156918c8673
rallao85@gmail.com <rallao85@gmail.com>
2021-11-23 22:03:10 -0300

    adds condition when user is logged in or logged out

--- Futures_Thinking/settings.py
+++ Futures_Thinking/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/feed'
+LOGOUT_REDIRECT_URL = '/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
