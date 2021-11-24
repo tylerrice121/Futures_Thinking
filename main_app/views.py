367d7fd6f8dbb1bdf1a65b1e5bfe69ac003f2f01
Tyler Rice <tylerrice121@gmail.com>
2021-11-24 10:04:23 -0600

    wip: create post form

--- main_app/views.py
+++ main_app/views.py
@@ -25,4 +25,8 @@ class Profile(ListView):
 
 class EntryCreate(CreateView):
     model = UserEntries
-    fields = '__all__'
+    fields = ('title', 'entry', 'img', 'video_url', 'date')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
