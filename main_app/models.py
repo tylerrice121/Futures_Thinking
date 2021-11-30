3cfc39f6dcc2e280c7ec8ace19cc71e4fcd9bc7b
rallao85@gmail.com <rallao85@gmail.com>
2021-11-30 10:31:04 -0300

    wip: adds email for login

--- main_app/models.py
+++ main_app/models.py
@@ -22,3 +22,7 @@ class AllEntries(models.Model):
 
     def __str__(self):
         return f"This user entry is from {self.user_entries}"
+
+# set email for login
+USERNAME_FIELD = 'email'
+REQUIRED_FIELDS = ['username']©
